d7bfb87c49a7a3af6368891415baa5c9aedad84d
topdev00000 <adrian.gortopan@gmail.com>
2021-02-27 10:34:38 +0900

    feature/project completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,19 +9,19 @@ import { authGuard } from './Guards/auth.guard';
 import { ProductdetailsComponent } from './components/productdetails/productdetails.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'home',canActivate:[authGuard] ,component:HomeComponent,title:'home'},
+  { path:'',redirectTo:'home',pathMatch:'full'},
+  { path:'home',canActivate:[authGuard] ,component:HomeComponent,title:'home'},
 
-  {path:'products/:id',canActivate:[authGuard],component:ProductsComponent,title:'products'},
-  {path:'productdetails/:id',canActivate:[authGuard],component:ProductdetailsComponent,title:'productdetails'},
-  {path:'categories',loadComponent:()=> import('./components/categories/categories.component').then(c=>c.CategoriesComponent),
+  { path:'products/:id',canActivate:[authGuard],component:ProductsComponent,title:'products'},
+  { path:'productdetails/:id',canActivate:[authGuard],component:ProductdetailsComponent,title:'productdetails'},
+  { path:'categories',loadComponent:()=> import('./components/categories/categories.component').then(c=>c.CategoriesComponent),
    canActivate:[authGuard],title:'categories'},
-  {path:'register',component:RegisterComponent,title:'register'},
-  {path:'login',component:LoginComponent,title:'login'},
-  {path:'notfound',component:NotfoundComponent,title:'notfound'},
+  { path:'register',component:RegisterComponent,title:'register'},
+  { path:'login',component:LoginComponent,title:'login'},
+  { path:'notfound',component:NotfoundComponent,title:'notfound'},
   { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
   { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) },
-  {path:'wishlist',loadComponent:()=>import('./components/wishlist/wishlist.component').then(c=>c.WishlistComponent),
+  { path:'wishlist',loadComponent:()=>import('./components/wishlist/wishlist.component').then(c=>c.WishlistComponent),
   canActivate:[authGuard],title:'wishlist'}
 
 ];
